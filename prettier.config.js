11830f2c782a4104151252789f7dc866fae5296b
yashien-lin <yashienxxxx@gmail.com>
2026-03-02 23:57:23 +0800

    chore: set up config to align ESLint with Prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  arrowParens: 'always',
+};
